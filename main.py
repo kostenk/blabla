3094200310ec5b0aba098ce4c3252486599801e7
Alexandr_Radchenko <sasha.radchenko97@gmail.com>
2022-05-19 12:41:13 +0300

    22

--- main.py
+++ main.py
@@ -4,8 +4,7 @@
 def print_hi(first):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-гоогогоогрнрн7
+vkdjkv
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
